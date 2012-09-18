bdb10500e808735aa94de095196fe4683c272763
Joost Verdoorn <joost@monster.(none)>
2012-09-18 16:37:02 +0200

    comm: Add stub communication php

---
+++ application/controllers/comm.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Comm extends CI_Controller {
+
+	public function convert()
+	{
+
+	}
+
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
