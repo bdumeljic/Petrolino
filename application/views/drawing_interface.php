d01597d846f66076a0b0db63aee0c1359a28b97a
Pieter ter Berg <info@pietertje.nl>
2012-09-18 16:28:58 +0200

    added images and basic js for transitions and places

--- application/views/drawing_interface.php
+++ application/views/drawing_interface.php
@@ -3,9 +3,11 @@
 	<h1>Drawing Interface</h1>
 	
 	<div class="btn-group">
-		<button class="btn"><i class="icon-asterisk"></i> Add transition</button>
-		<button class="btn"><i class="icon-map-marker"></i> Add place</button>
+		<button class="btn" id="createTransition"><i class="icon-asterisk"></i> Add transition</button>
+		<button class="btn" id="createPlace"><i class="icon-map-marker"></i> Add place</button>
 		<button class="btn"><i class="icon-arrow-right"></i> Add action</button>
 	</div>
+	
+	<canvas id="board" width="800" height="500"></canvas>
 
 <?include_once('include/footer.php');?>
--- assets/js/petrolino.js
+++ assets/js/petrolino.js
@@ -1,3 +1,44 @@
+// tmp js
+function createTransition() {
+
+	$("canvas#board").drawRect({
+		strokeStyle: "#000",
+		fillStyle: "#666",
+		strokeWidth: 1,
+		x: 150, y: 100,
+		width: 100,
+		height: 100,
+		cornerRadius: 5,
+		layer: true,
+		draggable: true,
+		mouseover: function() {
+			$(this).css({cursor: "pointer"});  
+		},
+		mouseout: function() {
+			$(this).css({cursor: "default"});  
+		},
+	});
+}
+
+function createPlace() {
+
+	$("canvas#board").drawArc({
+		strokeStyle: "#000",
+		fillStyle: "#c33",
+		strokeWidth: 1,
+		x: 180, y: 200,
+		radius: 50,
+		layer: true,
+		draggable: true,
+		mouseover: function() {
+			$(this).css({cursor: "pointer"});  
+		},
+		mouseout: function() {
+			$(this).css({cursor: "default"});  
+		},
+	});
+}
 $(document).ready(function() {
-	//javascript init
+	$("#createTransition").bind('click',createTransition)
+	$("#createPlace").bind('click',createPlace)
 })
