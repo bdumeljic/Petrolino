28a9dc62929cfafe742442cc41ca4459c13d4232
Joost Verdoorn <joost@monster.(none)>
2012-09-18 15:27:58 +0200

    git_hook: Some cleaing up.

--- application/controllers/git_hook.php
+++ application/controllers/git_hook.php
@@ -4,19 +4,7 @@ class Git_hook extends CI_Controller {
 
 	public function index()
 	{
-		//exec("/bin/bash");
-		//exec("cd /var/www/petrolino && git pull", $output);
-		
-		//$output = `cd /var/www/petrolino && git pull`;
 		echo nl2br(shell_exec('git pull 2>&1'));
-		
-		/*$output = `cd /var/www/`;
-		print_r($output);
-
-		$output = `ls -al`;
-		print_r($output);*/
-
-		echo "<br />Enough?";
 	}
 }	
 
