3ac98a850009997410790446189b5719f4d02b2a
Joost Verdoorn <joost@monster.(none)>
2012-09-18 15:53:09 +0200

    drawing_interface: Add icons

--- application/views/drawing_interface.php
+++ application/views/drawing_interface.php
@@ -1,11 +1,11 @@
 <?include_once('include/header.php');?>
 
-	<h1>drawing interface</h1>
+	<h1>Drawing Interface</h1>
 	
 	<div class="btn-group">
-		<button class="btn">[] add transition</button>
-		<button class="btn">O add place</button>
-		<button class="btn">-&gt; add action</button>
+		<button class="btn"><i class="icon-asterisk"></i> Add transition</button>
+		<button class="btn"><i class="icon-map-marker"></i> Add place</button>
+		<button class="btn"><i class="icon-arrow-right"></i> Add action</button>
 	</div>
 
 <?include_once('include/footer.php');?>
