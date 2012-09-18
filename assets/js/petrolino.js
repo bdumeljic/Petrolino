b69c7f01f3d7956d305384a0fc83ae11f2609f30
Pieter ter Berg <info@pietertje.nl>
2012-09-18 17:03:23 +0200

    some testing

--- assets/js/petrolino.js
+++ assets/js/petrolino.js
@@ -1,10 +1,34 @@
 // tmp js
+function dump(a) {
+	var acc = []
+	$.each(a, function(index, value) {
+	    acc.push(index + ': ' + value);
+	});
+	return (JSON.stringify(acc));
+}
+
+function mousePos(e) {
+	var x;
+	var y;
+	if (e.pageX || e.pageY) { 
+	  x = e.pageX;
+	  y = e.pageY;
+	}
+	else { 
+	  x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
+	  y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+	} 
+	// x -= gCanvasElement.offsetLeft;
+	// y -= gCanvasElement.offsetTop;
+	var out = [x,y];
+	return out;
+}
 function createTransition() {
 
 	$("canvas#board").drawRect({
 		strokeStyle: "#000",
 		fillStyle: "#666",
-		strokeWidth: 1,
+		strokeWidth: 3,
 		x: 150, y: 100,
 		width: 100,
 		height: 100,
@@ -12,10 +36,24 @@ function createTransition() {
 		layer: true,
 		draggable: true,
 		mouseover: function() {
-			$(this).css({cursor: "pointer"});  
+			$(this).css({cursor: "pointer"});
 		},
 		mouseout: function() {
 			$(this).css({cursor: "default"});  
+		},
+		click: function(e) {
+			$(this).addClass("active");  
+			// var myContext = $(this).parent();
+			// alert(tmp.fillStyle);
+			// tmp.fillStyle = "#f00";
+			// var tmp = mousePos(e);
+			alert(e.pageX +', '+ e.pageY);
+			// alert(dump(tmp));
+			var myCanvas = document.getElementById("board");
+			var myContext = myCanvas.getContext("2d");
+			myContext.moveTo(0,0);
+			myContext.quadraticCurveTo(0,100,100,250);
+			myContext.stroke();
 		},
 	});
 }
