1d29cd7ff000cf32c187ae7efff2ff2f452c957e
Joost Verdoorn <joost@monster.(none)>
2012-09-19 12:31:15 +0200

    Add tooltips to Place

--- assets/js/place.js
+++ assets/js/place.js
@@ -1,31 +1,67 @@
 function Place(x, y) {
-
-	if(!x) {
-		x = 150;
-	}
-	
-	if(!y) {
-		y = 150;
-	}
 	
 	this.init = function() {
-		this.draw(this.id, x, y);
-	}	
+		// Set x and y defaults
+		if(!x) {
+			x = 150;
+		}
+
+		if(!y) {
+			y = 150;
+		}
+		
+		// Generate unique id
+		this.id = "pl" + Drawboard.places.length;
+		
+		
+		// Build ToolTip
+		this.toolTip = $('<div class="popover right"><div class="arrow"></div><h3 class="popover-title">' + this.id + '</h3><div class="popover-content"><p>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p></div></div>');
+		
+		this.toolTip.css('position', 'absolute');
+		this.toolTip.css('display', 'none');
+		this.toolTip.css('z-index', '1000');		
+		
+		$('body').append(this.toolTip);
+		
+		// Draw the actor
+		this.actor = this.draw(this.id, x, y, this);
+	}
 	
-	this.id = "pl" + Drawboard.transitions.length;
+	this.getX = function() {
+		var x = this.actor.x;
+		console.log('x=' + x)
+		return x;
+		
+	}
 	
-	this.draw = function(id, x, y) {
+	this.getY = function() {
+		var y = this.actor.y;
+		console.log('y=' + y)
+		return y;
+	}
+	
+	this.toggleToolTip = function() {
+		this.toolTip.css({top: this.getY() - this.toolTip.height()/ 2 , left: this.getX() + 25});
+		this.toolTip.toggle();
+	}
+	
+	this.hideToolTip = function() {
+		this.toolTip.hide();
+	}
+
+	
+	this.draw = function(id, x, y, delegate) {
 		Drawboard.canvas.drawArc({
-			name: name,
+			name: id,
 		
 			strokeStyle: "#000",
 			fillStyle: "#c33",
-			strokeWidth: 3,
+			strokeWidth: 1,
 			cornerRadius: 5,
 		
 			x: x, 
 			y: y,
-			radius:50,	
+			radius:20,	
 		
 			layer: true,
 			draggable: true,
@@ -37,20 +73,23 @@ function Place(x, y) {
 				$(this).css({cursor: "default"});  
 			},
 			mousedown: function() {
-				console.log("Mouse down");
+				this.beingClicked = true;
 			},
 			mouseup: function() {
-				console.log("Mouse up");
+				if(this.beingClicked) {
+					delegate.toggleToolTip();	
+				}
 			},
 			click: function(e) {
-				$(this).addClass("active");  		
+				$(this).addClass("active");				
 			},
 			drag: function() {
-				console.log("Drag");
+				this.beingClicked = false;
+				delegate.hideToolTip();
 			}
 		});
 		
-		console.log("Transition drawn");
+		return Drawboard.canvas.getLayer(id);
 	};
 	
 	this.init();
