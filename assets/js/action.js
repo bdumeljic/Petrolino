bc4311dd62b664fbd5884efd30d5294e466fb160
Joost Verdoorn <joost@monster.(none)>
2012-09-19 14:42:34 +0200

    Complete PNML conversion

--- assets/js/action.js
+++ assets/js/action.js
@@ -5,7 +5,11 @@ function Action (source, target) {
 		this.target = t;
 
 		// Generate unique id
-		this.id = "pl" + Drawboard.actions.length;
+		this.id = 'a' + Drawboard.actions.length;
+		this.name = 'Action ' + Drawboard.actions.length;
+		
+		// Set description
+		this.description = "Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum."
 
 		// Draw the actor
 		this.actor = this.draw(this.id,this);
@@ -14,8 +18,8 @@ function Action (source, target) {
 	
 	
 	this.draw = function(id, delegate) {
-		var s = delegate.source;
-		var t = delegate.target;
+		var s = delegate.source.actor;
+		var t = delegate.target.actor;
 		
 		Drawboard.canvas.drawLine({
 			strokeStyle: "#000",
@@ -32,13 +36,29 @@ function Action (source, target) {
 	
 	this.update = function() {
 		var l = this.actor;
-		var s = this.source;
-		var t = this.target;
+		var s = this.source.actor;
+		var t = this.target.actor;
 		// console.log(l);
 		l.x1 = s.x;
 		l.y1 = s.y;
 		l.x2 = t.x;
 		l.y2 = t.y;
+	}
+	
+	this.toPNML = function() {
+		res  = '<arc id="' + this.id + '" source="' + this.source.id + '" target="' + this.target.id + '">\n';
+		
+		res += '	<name>\n';
+		res += '		<text>' + this.name + '</text>\n';
+		res += '	</name>\n';
+		
+		res += '	<description>\n';
+		res += '		<text>' + this.description + '</text>\n';
+		res += '	</description>\n';
+		
+		res += '</arc>\n';
+		
+		return res;
 	}
 	
 	this.init(source, target);
